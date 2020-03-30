f2d9aa50f00a04c0ecaa4faf7e0ff3fdfd07e408
Subham Kapoor <subham.kapoor@payufin.com>
2020-03-30 06:44:01 +0530

    correction for age stats function

--- ageteller/src/components/AgeStats.js
+++ ageteller/src/components/AgeStats.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import partyPopper from '../assets/party-popper.jpg';
 
 class AgeStats extends Component {
+
   timeSince(date) {
-    let today = new Date().getTime();
-    let other_date = new Date(date).getTime();
-    let difference = Math.abs(today - other_date);
+        var a = moment(new Date().getTime());
+        var b = moment(date);
 
-    let days = Math.floor(difference / (1000*3600*24));
-    let years = Math.floor(days/365);
-    days -= years*365;
-    let months = Math.floor(days/31);
-    days -= months*31;
+        var years = a.diff(b, 'year');
+        b.add(years, 'years');
 
-    return `${years} years, ${months} months, and ${days} days`
-  }
+        var months = a.diff(b, 'months');
+        b.add(months, 'months');
+
+        var days = a.diff(b, 'days');
+
+        console.log(years + ' years ' + months + ' months ' + days + ' days');
+        return `${years} years, ${months} months, and ${days} days`
+        }
 
   render() {
     return (
